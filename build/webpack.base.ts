926ce8f813d31687d50bb541f8fee83d0749f77f
jeff lang <langjinjie@tenacity.com.cn>
2023-04-18 19:05:29 +0800

    ci: copy 静态资源

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -1,4 +1,4 @@
-import { Configuration } from 'webpack' // 引入webpack
+import { Configuration, DefinePlugin } from 'webpack' // 引入webpack
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 const path = require('path') // 需要安装@types/node -D
 
@@ -32,14 +32,26 @@ const baseConfig: Configuration = {
   // plugin 的配置
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'webpack5-react-ts',
+      filename: 'index.html',
       // 复制 index.html文件，并自动引入打包输出的所有资源(js/css)
       template: path.join(__dirname,'../public/index.html'),
+      inject: true, // 自动注入静态资源, 一般在有多个人口文件的时候才会设置为false
+      hash: true,
       // 压缩html资源
       minify:{
+        removeAttributeQuotes: true, // 删除HTML中属性值周围的引号
         collapseWhitespace: true, // 去空格
-        removeComments: true // 去注释
+        removeComments: true, // 去注释
+        minifyJS: true, // 在脚本元素和事件属性中缩小JavaScript
+        minifyCSS: true, // 缩小css样式元素和样式属性
       }
-    })
+    }),
+    // new DefinePlugin({
+    //   "process.env": JSON.stringify(envConfig.parsed),
+    //   "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
+    //   "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    // })
   ]
 }
 // 导出
--- build/webpack.prod.ts
+++ build/webpack.prod.ts
@@ -1,13 +1,27 @@
-import { Configuration } from 'webpack' // 引入webpack的类型接口
+import path from 'path'
+import { Configuration, sources } from 'webpack' // 引入webpack的类型接口
 import { merge } from 'webpack-merge'
 import baseConfig from './webpack.base' // 引入基本配置
+import CopyPlugin  from 'copy-webpack-plugin'
 
 const prodConfig: Configuration = merge(baseConfig, {
-  mode:'production' // 生产模式，会开启tree-shaking和压缩代码，以及其他优化
+  mode:'production', // 生产模式，会开启tree-shaking和压缩代码，以及其他优化
   /* 
   打包环境推荐：none（不配置devTool选项了，不是配置devTool:"none"）
   在package.json的scripts中添加  "build": "webpack -c build/webpack.prod.ts"
   */
+  plugins:[
+    // 处理生产的
+    new CopyPlugin({
+      patterns:[
+        {
+          from: path.join(__dirname, '../public'), // 静态文件入口
+          to: path.join(__dirname, '../dist'), // 静态文件输出
+          filter:(sources)=> !sources.includes('index.html') // 过滤掉 index.html
+        }
+      ]
+    })
+  ]
 })
 
 export default prodConfig
